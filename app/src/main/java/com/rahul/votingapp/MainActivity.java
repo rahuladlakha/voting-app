ac9036c628cbaa8ebf69f04df86b7644bff004d3
Rahul Adlakha <rahuladlakha26@gmail.com>
2021-12-08 17:24:43 +0530

    Added bottom navigation menu and also initiated it with the required fragments.
    The bottom navigation bar is up and running now with three fragments titled - Home, Results, and Profile.

--- app/src/main/java/com/rahul/votingapp/MainActivity.java
+++ app/src/main/java/com/rahul/votingapp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.rahul.votingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((BottomNavigationView)findViewById(R.id.bottomNavigationView))
+                .setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        int id = item.getItemId();
+                        switch (id) {
+                            case R.id.homeItem:
+                                setFragment(new HomeFragment());
+                                break;
+                            case R.id.resultItem:
+                                setFragment(new ResultsFragment());
+                                break;
+                            case R.id.profileItem:
+                                setFragment(new ProfileFragment());
+                                break;
+                        }
+                        return true;
+                    }
+                });
+    }
+    private void setFragment(Fragment fragment){
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContainerView,fragment)
+                .commit();
     }
 }
