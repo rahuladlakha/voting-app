8e4a622516ac8eb960b0ed7fcf9059f5547972e6
Rahul Adlakha <rahuladlakha26@gmail.com>
2021-12-16 11:16:12 +0530

    removed issue - after signout from profile fragment, app was coming back on profile fragment after pressing back button on signin activity.
    Have just added a finish() command in the mainactivity after signing out.

--- app/src/main/java/com/rahul/votingapp/MainActivity.java
+++ app/src/main/java/com/rahul/votingapp/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Intent intent = new Intent(this, SignInActivity.class);
             startActivity(intent);
+            finish();
         }
     }
     @Override
