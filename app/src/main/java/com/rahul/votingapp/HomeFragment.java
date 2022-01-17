d29efb5949544aa9ce6ded775b553ad6dc4b6979
Rahul Adlakha <rahuladlakha26@gmail.com>
2022-01-17 21:03:02 +0530

    Created the UI of NewPollActivity
    The activity now has all the required edittext fields. A maximum of 10 options are allowed for the time being

--- app/src/main/java/com/rahul/votingapp/HomeFragment.java
+++ app/src/main/java/com/rahul/votingapp/HomeFragment.java
@@ -3,6 +3,8 @@ package com.rahul.votingapp;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -17,19 +19,30 @@ public class HomeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        fabNewPoll = (FloatingActionButton)getActivity().findViewById(R.id.fabNewPoll);
-        fabNewPoll.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getContext(), NewPollActivity.class);
-                startActivity(intent);
-            }
-        });
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        fabNewPoll = (FloatingActionButton) getActivity().findViewById(R.id.fabNewPoll);
+        fabNewPoll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fabNewPoll.animate().rotationBy(90).setDuration(1000).withEndAction(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(getContext(), NewPollActivity.class);
+                        startActivity(intent);
+                    }
+                });
+            }
+        });
     }
 }
--- app/src/main/java/com/rahul/votingapp/NewPollActivity.java
+++ app/src/main/java/com/rahul/votingapp/NewPollActivity.java
@@ -1,30 +1,56 @@
 package com.rahul.votingapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class NewPollActivity extends AppCompatActivity {
     Button btnAddOption;
-    EditText Option1, Option2, Option3, Option4, Option5, Option6, Option7, Option8, Option9, Option10;
-    Integer count = 3;
+    EditText Option[] = new EditText[11];
+    Integer optionCount = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_poll);
+        getSupportActionBar().hide();
         btnAddOption = findViewById(R.id.btnAddOption);
-        Option1 = findViewById(R.id.eTOption1);
-        Option2 = findViewById(R.id.eTOption2);
-        Option3 = findViewById(R.id.eTOption3);
-        Option4 = findViewById(R.id.eTOption4);
-        Option5 = findViewById(R.id.eTOption5);
-        Option6 = findViewById(R.id.eTOption6);
-        Option7 = findViewById(R.id.eTOption7);
-        Option8 = findViewById(R.id.eTOption8);
-        Option9 = findViewById(R.id.eTOption9);
-        Option10 = findViewById(R.id.eTOption10);
+        Option[1] = findViewById(R.id.eTOption1);
+        Option[2] = findViewById(R.id.eTOption2);
+        Option[3] = findViewById(R.id.eTOption3);
+        Option[4] = findViewById(R.id.eTOption4);
+        Option[5] = findViewById(R.id.eTOption5);
+        Option[6] = findViewById(R.id.eTOption6);
+        Option[7] = findViewById(R.id.eTOption7);
+        Option[8] = findViewById(R.id.eTOption8);
+        Option[9] = findViewById(R.id.eTOption9);
+        Option[10] = findViewById(R.id.eTOption10);
+        btnAddOption.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (++optionCount > 10){
+                    Toast.makeText(NewPollActivity.this, "A maximum of 10 options are allowed !", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Option[optionCount].setVisibility(View.VISIBLE);
+            }
+        });
+        findViewById(R.id.fabSavePoll).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(NewPollActivity.this, "Your poll was saved !",Toast.LENGTH_SHORT).show();
+                NewPollActivity.this.finish();
+            }
+        });
     }
+
+
 }
