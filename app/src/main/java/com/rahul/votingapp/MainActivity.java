fc62f187459dfa1f2aa6d3c9db905c518a027fa0
Rahul Adlakha <rahuladlakha26@gmail.com>
2022-01-21 11:25:35 +0530

    The app can now be opened from link now. Added an intent-filter to make the app work with links. The functionality to fetch the poll with the help of link is yet to be added. The link format is - "https://vote.com/<Poll access code>"

--- app/src/main/java/com/rahul/votingapp/MainActivity.java
+++ app/src/main/java/com/rahul/votingapp/MainActivity.java
@@ -48,6 +48,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // The code below makes the app open through links and also retrieve the poll code from link
+        Intent intent = getIntent();
+        if (intent.getAction() == Intent.ACTION_VIEW){
+            String[] ar = intent.getData().toString().trim().split("/");
+            String pollCode = ar[ar.length-1]; // Note that therre may be cases when pollCode was not even included in the link.
+            Toast.makeText(this, pollCode,Toast.LENGTH_LONG).show();
+
+        }
+
         dbRef = FirebaseDatabase.getInstance().getReference();
         getSignInInfo();
 
